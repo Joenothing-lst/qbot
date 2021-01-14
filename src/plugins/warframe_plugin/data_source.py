b3e914eb725cc442aec914bc9fec3b77d5547fec
liushuntian <912871833@qq.com>
2021-01-14 16:54:54 +0800

    解耦

--- src/plugins/warframe_plugin/data_source.py
+++ src/plugins/warframe_plugin/data_source.py
@@ -1,15 +1,10 @@
-import httpx
+from src.utils.util import async_request
 
 
 wm_api = 'http://nymph.rbq.life:3000/wm/robot/{}'
 rm_api = 'http://nymph.rbq.life:3000/rm/robot/{}'
 wiki_api = 'http://nymph.rbq.life:3000/wiki/robot/{}'
 
-
-async def async_request(method: str, url: str, **kwargs):
-    async with httpx.AsyncClient() as client:
-        result = await client.request(method.upper(), url, **kwargs)
-        return result
 
 async def get_wm_item(item):
     url = wm_api.format(item)
--- src/utils/util.py
+++ src/utils/util.py
@@ -1,6 +1,14 @@
+import httpx
+
 from nonebot import get_bots
 
 
 def get_bot():
     if get_bots():
         return list(get_bots().values())[0]
+
+
+async def async_request(method: str, url: str, **kwargs):
+    async with httpx.AsyncClient() as client:
+        result = await client.request(method.upper(), url, **kwargs)
+        return result
