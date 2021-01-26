dc7fb227490203ac3d6553857d7533525d5fbde9
liushuntian <912871833@qq.com>
2021-01-26 17:37:49 +0800

    feature

--- src/plugins/word_bank/__init__.py
+++ src/plugins/word_bank/__init__.py
@@ -1,7 +1,7 @@
 import re
 import random
 
-from nonebot import on_command, on_message
+from nonebot import on_command, on_message, export
 from nonebot.permission import SUPERUSER
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.message import Message
@@ -10,7 +10,9 @@ from nonebot.adapters.cqhttp.utils import unescape
 from nonebot.adapters.cqhttp.permission import GROUP_OWNER, GROUP_ADMIN, PRIVATE_FRIEND
 
 from .data_source import word_bank as wb
+from .util import parse_at, parse_cmd
 
+export().word_bank = wb
 
 wb_matcher = on_message(priority=99)
 
@@ -25,7 +27,8 @@ async def _(bot: Bot, event: MessageEvent):
         await bot.send(event, message=Message(unescape(random.choice(msg))))
 
 
-wb_set_cmd = on_command('问', permission=GROUP_ADMIN | GROUP_OWNER | PRIVATE_FRIEND | SUPERUSER, aliases={'全局问', '全局模糊问', '模糊问', })
+wb_set_cmd = on_command('问', permission=GROUP_ADMIN | GROUP_OWNER | PRIVATE_FRIEND | SUPERUSER,
+                        aliases={'全局问', '全局模糊问', '模糊问', })
 
 @wb_set_cmd.handle()
 async def wb_set(bot: Bot, event: MessageEvent):
