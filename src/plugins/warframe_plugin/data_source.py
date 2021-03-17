b719d0c984c7da6bc5b91a94f6934b3a6c8b9b79
liushuntian <912871833@qq.com>
2021-03-17 11:25:01 +0800

    :bug: fix data_source

--- src/plugins/warframe_plugin/data_source.py
+++ src/plugins/warframe_plugin/data_source.py
@@ -8,26 +8,26 @@ rm_api = 'http://{}/rm/robot/{}'
 wiki_api = 'http://{}/wiki/robot/{}'
 
 dic = {
-    '服务器时间': 'cambionCycle',
-    '新闻': 'cambionCycle',
-    '活动': 'cambionCycle',
-    '警报': 'cambionCycle',
-    '突击': 'cambionCycle',
-    '地球赏金': 'cambionCycle',
-    '金星赏金': 'cambionCycle',
-    '火卫二赏': 'cambionCycle',
-    '裂缝': 'cambionCycle',
-    '促销商品': 'cambionCycle',
-    '入侵': 'cambionCycle',
-    '奸商': 'cambionCycle',
-    '达尔沃': 'cambionCycle',
-    '小小黑': 'cambionCycle',
-    '地球': 'cambionCycle',
-    '地球平原': 'cambionCycle',
-    '舰队': 'cambionCycle',
-    '金星平原': 'cambionCycle',
-    '电波': 'cambionCycle',
-    '仲裁': 'cambionCycle',
+    '服务器时间': 'timestamp',
+    '新闻': 'news',
+    '活动': 'events',
+    '警报': 'alerts',
+    '突击': 'sortie',
+    '地球赏金': 'Ostrons:',
+    '金星赏金': 'Solaris',
+    '火卫二赏': 'EntratiSyndicate:',
+    '裂缝': 'fissures',
+    '促销商品': 'flashSales',
+    '入侵': 'invasions',
+    '奸商': 'voidTrader',
+    '达尔沃': 'dailyDeals',
+    '小小黑': 'persistentEnemies',
+    '地球': 'earthCycle',
+    '地球平原': 'cetusCycle',
+    '舰队': 'constructionProgress',
+    '金星平原': 'vallisCycle',
+    '电波': 'nightwave',
+    '仲裁': 'arbitration',
     '火卫二平原': 'cambionCycle'
 }
 
@@ -39,6 +39,7 @@ async def get_wf_item(msg):
         return results.text
     else:
         return '未找到【{msg}】，请从下列选项中选择查询：\n' + '\n'.join(i for i in dic.values())
+
 
 async def get_wm_item(item):
     url = wm_api.format(HOST, item)
