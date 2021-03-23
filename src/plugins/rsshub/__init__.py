a2832e2215e1c5aa6b0b6336f4e623c152ff9017
liushuntian <912871833@qq.com>
2021-03-23 15:07:09 +0800

    :art: style fixed

--- src/plugins/rsshub/__init__.py
+++ src/plugins/rsshub/__init__.py
@@ -36,7 +36,6 @@ temp = "\n【{tags}】{title}\n{link}"
 
 rss_chouti = RssHub('https://dig.chouti.com/feed.xml', temp, headers=headers1)
 rss_chouti_hot = RssHub('https://rsshub.app/chouti/hot', temp)
-rss_chouti_top = RssHub('https://rsshub.app/chouti/top/24', temp)
 rss_agree = RssHub(
     'http://www.galgamezs.net/bbs/rss.php?fid=8&auth=7922Wq3mDt%2FPddHPHDb%2BMHiWp%2FP3Xj9Uv56wSMHEoFweHME5gOzqdH08cXQCV1k',
     headers=headers2
