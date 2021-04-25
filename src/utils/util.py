c8ff11d2143206a9c9150558f77fc8f77b81be6e
liushuntian <912871833@qq.com>
2021-04-25 14:33:16 +0800

    util fixed

--- src/utils/util.py
+++ src/utils/util.py
@@ -1,4 +1,5 @@
 import httpx
+import asyncio
 
 from nonebot import get_bots
 from nonebot.rule import ArgumentParser
@@ -23,3 +24,18 @@ base_parser.add_argument('-h', dest='help', action='store_true', default=False)
 
 def gen_parser():
     return ArgumentParser(add_help=False, parents=[base_parser])
+
+
+def get_loop():
+    return asyncio.get_running_loop()
+
+
+def wrapper(api: str, params: dict):
+    loop = get_loop()
+    bot = get_bot()
+    loop.create_task(bot.call_api(api, **params))
+
+
+async def call_api_delay(api: str, delay: float, **params):
+    loop = get_loop()
+    loop.call_later(delay, wrapper, api, params)
