4a6b3f2501a9af8a4c67a137295293d90f19fc6d
liushuntian <912871833@qq.com>
2021-04-25 15:05:34 +0800

    rebuild

--- src/plugins/ghs/__init__.py
+++ src/plugins/ghs/__init__.py
@@ -7,23 +7,29 @@ from nonebot.adapters.cqhttp.message import Message, MessageSegment
 from src.utils.util import call_api_delay
 from .data_source import search_r18book, Book, gen_forward_message
 
+r18book_search = on_command('搜本子', aliases={'找本子', })
 
-r18book_search = on_command('搜本子', aliases={'找本子',})
 
 @r18book_search.handle()
-async def _(bot: Bot, event: MessageEvent):
+async def _(bot: Bot, event: GroupMessageEvent):
     params = unescape(str(event.message))
-    
-    books = search_r18book(params)
-    msg = f'找到关键词 【{params}】的本子：'
-    msg = msg + '\n'.join(f"\nid：{book['book_id']}\n名称：{book['title']}" for book in books)
 
-    msg_id = await bot.send(event, msg)
+    books = search_r18book(params)
+    msg = f'找到关键词【{params}】的本子'
+    # msg = msg + '\n'.join(f"\nid：{book['book_id']}\n名称：{book['title']}" for book in books)
+
+    forward_msg = gen_forward_message(
+        [msg] + [f"id：{book['book_id']}\n名称：{book['title']}\n{MessageSegment.image(file=book['cover'])}" for book in
+                 books],
+        event.sender.user_id)
+
+    msg_id = await bot.call_api('send_group_forward_msg', group_id=event.group_id, messages=Message(forward_msg))
 
     await call_api_delay(api='delete_msg', delay=30, message_id=msg_id.get('message_id'))
 
 
 r18book_view = on_command('看本子')
+
 
 @r18book_view.handle()
 async def _(bot: Bot, event: GroupMessageEvent):
