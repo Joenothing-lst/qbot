1edfbf448be8eb354b801bbd586327934e22de36
liushuntian <912871833@qq.com>
2021-06-15 15:29:29 +0800

    update hs plugin

--- src/plugins/hearthstone_plugin/__init__.py
+++ src/plugins/hearthstone_plugin/__init__.py
@@ -1,13 +1,14 @@
 import re
 
-from nonebot import on_message
-from nonebot.rule import command, Rule
+from nonebot import on_message, on_command
+from nonebot.rule import Rule
 from nonebot.permission import SUPERUSER
 from nonebot.typing import T_State
 from nonebot.adapters import Event
+from nonebot.adapters import Bot as BaseBot
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.message import Message
-from nonebot.adapters.cqhttp.event import MessageEvent, GroupMessageEvent, GroupRequestEvent
+from nonebot.adapters.cqhttp.event import MessageEvent
 
 from .data_source import HSHandle
 
@@ -15,8 +16,7 @@ hs = HSHandle()
 
 
 def check_base64() -> Rule:
-
-    async def _check_base64(bot: Bot, event: Event, state: T_State) -> bool:
+    async def _check_base64(bot: BaseBot, event: Event, state: T_State) -> bool:
         msg = str(event.get_message())
         p = '^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$'
         try:
@@ -27,6 +27,7 @@ def check_base64() -> Rule:
 
     return Rule(_check_base64)
 
+
 hs_api = on_message(rule=check_base64(), permission=SUPERUSER)
 
 
@@ -36,6 +37,6 @@ async def _(bot: Bot, event: MessageEvent):
     try:
         data, hero, format_type = hs.get_deck_from_deckstring(msg)
         msg = f"{format_type}模式\n{hero}\n" + '\n'.join(f"{v} * {k}" for _, k, v in data)
-        await bot.send(event, msg)
+        await bot.send(event, Message(msg))
     except:
         pass
