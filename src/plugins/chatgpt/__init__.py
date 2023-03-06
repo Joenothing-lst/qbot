090967bfdc08b788f818e20ac645058ca882ec46
lst-dev <912871833@qq.com>
2023-03-06 10:20:50 +0800

    :sparkles: add openai chatgpt

---
+++ src/plugins/chatgpt/__init__.py
@@ -0,0 +1,26 @@
+from nonebot import on_message
+# from nonebot.permission import SUPERUSER
+from nonebot.typing import T_State
+from nonebot.adapters.cqhttp.bot import Bot
+from nonebot.adapters.cqhttp.event import MessageEvent, GroupMessageEvent
+from nonebot.adapters.cqhttp.utils import unescape, escape
+from nonebot.adapters.cqhttp.message import Message, MessageSegment
+
+from src.plugins.chatgpt.data_source import chat
+
+
+
+chatgpt = on_message()
+
+
+@chatgpt.handle()
+async def _(bot: Bot, event: MessageEvent):
+    if event.to_me:
+        print(bot.config.__dict__)
+        token = bot.config.PERSONAL_API_KEY
+        uid = event.user_id
+        msg = unescape(str(event.message))
+        print(token, uid, msg)
+        reply = chat(token, uid, msg)
+        # await bot.send(event, Message(reply))
+
---
+++ src/plugins/chatgpt/data_source.py
@@ -0,0 +1,8 @@
+import requests
+
+
+def chat(token, uid, str):
+    # url = f"http://127.0.0.1/OpenaiApi/chatgpt/chat?token={token}&uid={uid}&message={str}"
+    # res = requests.get(url)
+    # return res.json()['data']['message']
+    return "test"
