62afcf9e512f28344f5bfe9a81d324b2d97a07b4
lst-dev <912871833@qq.com>
2023-03-29 18:05:42 +0800

    :art: add live feature

--- src/plugins/chatgpt/__init__.py
+++ src/plugins/chatgpt/__init__.py
@@ -5,24 +5,29 @@ from nonebot.adapters.cqhttp.event import MessageEvent
 from nonebot.adapters.cqhttp.message import Message, MessageSegment
 from nonebot.adapters.cqhttp.utils import unescape
 
-from .data_source import chat, is_user
+from .data_source import chat, is_user_living, set_user_living, clean_context
 
 chatgpt = on_message(priority=98, block=False)
 
 
 @chatgpt.handle()
 async def _(bot: Bot, event: MessageEvent):
-    if event.to_me:
-        token = bot.config.personal_api_key
-        uid = event.user_id
-        msg = unescape(str(event.message))
-        if any(i in msg for i in ['assistant', 'vanilla', 'makise', 'pcr_kokoro']):
-            reply = chat(token, uid, msg)
-            await bot.send(event, Message(reply))
+    token = bot.config.personal_api_key
+    uid = event.user_id
+    msg = unescape(str(event.message))
+    if event.to_me and any(i in msg for i in ['assistant', 'vanilla', 'makise', 'pcr_kokoro']):
+        # 创建用户/角色
+        reply = chat(token, uid, msg)
+        await bot.send(event, Message(reply))
 
-        elif is_user(token, uid):
-            reply = chat(token, uid, msg)
-            reply_msg = MessageSegment.text(reply)
-            if event.message_type == 'group':
-                reply_msg = MessageSegment.reply(event.message_id) + reply_msg
-            await bot.send(event, reply_msg)
+    elif '结束对话' in msg:
+        # 结束用户的对话
+        if clean_context(token, uid) == 'success':
+            await bot.send(event, '已结束对话')
+
+    elif (event.to_me and set_user_living(token, uid) == 'success') or is_user_living(token, uid):
+        reply = chat(token, uid, msg)
+        reply_msg = MessageSegment.text(reply)
+        if event.message_type == 'group':
+            reply_msg = MessageSegment.reply(event.message_id) + reply_msg
+        await bot.send(event, reply_msg)
--- src/plugins/chatgpt/data_source.py
+++ src/plugins/chatgpt/data_source.py
@@ -11,11 +11,31 @@ def chat(token, uid, str):
         return res['msg']
 
 
-def is_user(token, uid):
-    url = f"http://127.0.0.1/OpenaiApi/chatgpt/is_user?token={token}&uid={uid}"
+def is_user_living(token, uid):
+    url = f"http://127.0.0.1/OpenaiApi/chatgpt/is_user_living?token={token}&uid={uid}"
     res = requests.get(url).json()
 
     if res['code'] == 200:
-        return res['data']['is_user']
+        return res['data']['is_user_living']
+    else:
+        return res['msg']
+
+
+def set_user_living(token, uid):
+    url = f"http://127.0.0.1/OpenaiApi/chatgpt/set_user_live?token={token}&uid={uid}&is_living=ture"
+    res = requests.get(url).json()
+
+    if res['code'] == 200:
+        return res['msg']
+    else:
+        return res['msg']
+
+
+def clean_context(token, uid):
+    url = f"http://127.0.0.1/OpenaiApi/chatgpt/clean_context?token={token}&uid={uid}"
+    res = requests.get(url).json()
+
+    if res['code'] == 200:
+        return res['msg']
     else:
         return res['msg']
