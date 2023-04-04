d14cdbf33f7420246dd751c740e455d26585ddb6
lst-dev <912871833@qq.com>
2023-04-04 16:16:38 +0800

    :art: get current msg

--- src/plugins/chatgpt/__init__.py
+++ src/plugins/chatgpt/__init__.py
@@ -17,27 +17,27 @@ async def _(bot: Bot, event: MessageEvent):
     if PLUGIN_ON:
         token = bot.config.personal_api_key
         uid = event.user_id
-        msg = unescape(str(event.message))
-
-        if event.to_me and any(i in msg for i in ['assistant', 'vanilla', 'makise', 'pcr_kokoro']):
-            # 创建用户/角色
-            reply = chat(token, uid, msg)
-            await bot.send(event, Message(reply))
-
-        elif '结束对话' in msg:
-            # 结束用户的对话
-            if set_user_living(token, uid, False) == 'success':
-                await bot.send(event, 'OK~')
-
-        elif is_user(token, uid):
-            if event.to_me or is_user_living(token, uid):
+        msg = unescape(str(event.get_plaintext()))
+        if msg:
+            if event.to_me and any(i in msg for i in ['assistant', 'vanilla', 'makise', 'pcr_kokoro']):
+                # 创建用户/角色
                 reply = chat(token, uid, msg)
-                if event.to_me:
-                    set_user_living(token, uid)
-                reply_msg = MessageSegment.text(reply)
-                if event.message_type == 'group':
-                    reply_msg = MessageSegment.reply(event.message_id) + reply_msg
-                await bot.send(event, reply_msg)
+                await bot.send(event, Message(reply))
+
+            elif '结束对话' in msg:
+                # 结束用户的对话
+                if set_user_living(token, uid, False) == 'success':
+                    await bot.send(event, 'OK~')
+
+            elif is_user(token, uid):
+                if event.to_me or is_user_living(token, uid):
+                    reply = chat(token, uid, msg)
+                    if event.to_me:
+                        set_user_living(token, uid)
+                    reply_msg = MessageSegment.text(reply)
+                    if event.message_type == 'group':
+                        reply_msg = MessageSegment.reply(event.message_id) + reply_msg
+                    await bot.send(event, reply_msg)
 
 
 chatgpt_turn = on_command('chatgpt', permission=SUPERUSER)
